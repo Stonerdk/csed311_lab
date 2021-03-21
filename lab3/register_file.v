b38a66f53273f2e8e8df7fb0b7f17bbe25ee0365
stonerdk <stkd97@gmail.com>
2021-03-21 23:15:44 +0900

    register file with reset_n

--- lab3/register_file.v
+++ lab3/register_file.v
@@ -1,4 +1,4 @@
-module register_file( read_out1, read_out2, read1, read2, write_reg, write_data, reg_write, clk); 
+module register_file( read_out1, read_out2, read1, read2, write_reg, write_data, reg_write, clk, reset_n); 
     output [15:0] read_out1;
     output [15:0] read_out2;
     input [1:0] read1;
@@ -7,15 +7,25 @@ module register_file( read_out1, read_out2, read1, read2, write_reg, write_data,
     input [15:0] write_data;
     input reg_write;
     input clk;
+    input reset_n;
 
     reg [3:0] register [15:0];
     
     assign read_out1 = register[read1];
     assign read_out2 = register[read2];
 
-    always @(posedge clk) begin
-        if (reg_write == 1'b1)
-            register[write_reg] = write_data;
+    always @(posedge clk or posedge reset_n) begin
+        if (reset == 1'b1) begin
+            register[0] <= 16'b0;
+            register[1] <= 16'b0;
+            register[2] <= 16'b0;
+            register[3] <= 16'b0;
+        end
+        else begin 
+            if (reg_write == 1'b1)
+                register[write_reg] <= write_data;
+        end
+        
     end
     // TODO : implement reset_n to initialization
 endmodule
