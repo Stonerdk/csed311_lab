f6dd4901aa817ebe860737a29e865bd8d651e001
stonerdk <stkd97@gmail.com>
2021-03-21 22:56:54 +0900

    implemented ALU

--- lab3/alu.v
+++ lab3/alu.v
@@ -8,4 +8,34 @@ module alu (alu_input_1, alu_input_2, func_code, alu_output);
 	input [2:0] func_code;
 	output reg [`NumBits-1:0] alu_output;
 
+	always @(func_code or alu_input_1 or alu_input_2) begin
+		case (func_code)
+		`FUNC_ADD: begin
+			alu_output <= alu_input_1 + alu_input_2;
+		end
+		`FUNC_SUB: begin
+			alu_output <= alu_input_1 - alu_input_2; // change order?
+		end
+		`FUNC_AND: begin
+			alu_output <= alu_input_1 & alu_input_2;
+		end
+		`FUNC_ORR: begin
+			alu_output <= alu_input_1 | alu_input_2;
+		end
+		`FUNC_NOT: begin
+			alu_output <= ~alu_input_1;
+		end
+		`FUNC_TCP: begin
+			alu_output <= ~alu_input_1 + 1;
+		end
+		`FUNC_SHL: begin
+			alu_output <= alu_input_1 << 1;
+		end
+		`FUNC_SHR: begin
+			alu_output <= alu_input_1 >> 1;
+		end
+		default: begin
+			alu_output <= alu_input_1;
+		end
+	end
 endmodule
