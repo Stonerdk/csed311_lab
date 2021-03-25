0ca57076d7e1f5cbfe0146cfea7e434a076eb6d4
4Rem <bhyu418@postech.ac.kr>
2021-03-26 05:14:45 +0900

    c

--- control_unit.v
+++ control_unit.v
@@ -14,24 +14,18 @@ module control_unit (instr, alu_src, reg_write, mem_read, mem_to_reg, mem_write,
     reg[5:0] funccode;
     opcode = instr[`WORD_SIZE:`WORD_SIZE - 3];
     funccode = instr[5:0];
-    
-    reg_write = (opcode != `BNE_OP) &&
-                (opcode != `BEQ_OP) &&
-                (opcode != `BGZ_OP) &&
-                (opcode != `BLZ_OP) &&
-                (opcode != `SWD_OP);
+
     alu_src = (opcode == `ADI_OP) ||
               (opcode == `ORI_OP) ||
               (opcode == `LHI_OP) ||
               (opcode == `LWD_OP) ||
               (opcode == `SWD_OP) ||
               (opcode == `JMP_OP) ||
-              (opcode == `JAL_OP);
-    mem_read = (opcode == `LWD_OP);
-    mem_write = (opcode == `SWD_OP);
-    jp = (opcode == `JMP_OP) ||
-         (opcode == `JAL_OP) ||
-         ((opcode == `ALU_OP) && ((funccode == `INST_FUNC_JPR) || (funccode == `INST_FUNC_JRL)));
-    branch = (opcode[3:2] == 0);
+              (opcode == `JAL_OP); //alu에서 immediate를 sr2로 사용할 때.
+
+    mem_read = (opcode == `LWD_OP); // memory에서 데이터를 읽어올 때.
+    mem_write = (opcode == `SWD_OP); // memory에 데이터를 저장할 때.
+
+
 
 endmodule
